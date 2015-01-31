51bb687110d6f69bfaa429266db430650c0a5a38
Kash Krishna <Kashys5@gmail.com>
2015-01-31 11:21:51 -0800

    made changes started adding firebase

---
+++ javascripts/game.js
@@ -0,0 +1,21 @@
+﻿
+
+var gameOn = true;
+
+
+
+while (true)
+{
+    var player = {
+        name: "player",
+        status: "healthy"
+    }
+
+    var ready_list = [];
+    var playerNum = 0;
+    while (gameOn)
+    {
+
+    }
+
+}
