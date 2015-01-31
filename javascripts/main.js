021ec17305148e150f5a259f2210b9fa5295f829
Kash Krishna <Kashys5@gmail.com>
2015-01-30 19:41:19 -0800

    Title Change

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,42 @@
 console.log('This would be the main JS file.');
+{
+
+    "NotificationSubscription": {
+
+        "name": "presence-event-trigger",
+
+        "notificationType": "EVENT_DRIVEN",
+
+        "dataFormat": "JSON",
+
+        "NotificationReceiverInfo": {
+
+            "transport": {
+
+                "type" : "TransportHttp",
+
+                "hostAddress": "1.2.3.4",
+
+                "port": "5555"
+
+            }
+
+        },
+
+    "subscribedEvents" : [
+
+    {
+
+        "type" : "PresenceEventTrigger",
+
+        "existingPresence" : "true",
+
+        "eventEntity": "WIRELESS_CLIENTS"
+
+    }
+
+    ]
+
+    }
+
+}
